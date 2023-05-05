08955d1323199dfe885c55fde65c3b558bf3e810
Anthony Cheetham <cheetham89@outlook.com>
2023-05-04 20:46:47 -0700

    made changes to everything

---
+++ playground.py
@@ -0,0 +1,13 @@
+
+# # Can I use bits usefully?
+# inds = [1,0,1,1,1]
+# x = 0
+# for ix,i in enumerate(inds):
+#     x += i*2**ix
+# def bitn(x,n):
+#     return x>>n&1
+
+# for n in range(5):
+#     print(bitn(x,n))
+
+print(tuple({5,3,1,7,8,999}))
